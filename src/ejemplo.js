c5f4baa47844676eb4d6cb0027ce1d523395375c
Andrea Rodriguez Rivares <alu0100826999@ull.edu.es>
2017-03-15 15:42:37 +0000

    Ejemplo en carpeta src

--- src/ejemplo.js
+++ src/ejemplo.js
@@ -1,27 +1,47 @@
-var cookie = require('cookie')
-var http = require('http');
-var url = require('url')
-//var cookieSession = require('cookie-session')
-var cookieParser = require("cookie-parser")
-var express = require('express')
-var app = express()
-var path = require('path');
-
-//Set-Cookie: <cookie-name>=<cookie-value>;
-
-document.cookie = nombrecookie=valorcookie; 
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'ejs');
-
-app.use(express.static('./'));
-
-app.get('/hi', function (req, res) {
-    res.send('Hello World')
-})
-
-var server = app.listen(8088, function () {
-  var host = server.address().address
-  var port = server.address().port
-  console.log('Example app listening at http://%s:%s', host, port)
+//Importamos los modulos express y express-session. Creamos una variable de express (app) y una de sesión (session).
+var express = require('express'),
+    app = express(),
+    session = require('express-session');
+app.use(session({
+    secret: '2C44-4D44-WppQ38S',
+    resave: true,
+    saveUninitialized: true
+}));
+ 
+//Función Middleware de Autenticación y Autorización. 
+//Concede el siguiente paso si el usuario es amy y si tiene acceso de administrador.
+var auth = function(req, res, next) {
+  if (req.session && req.session.user === "amy" && req.session.admin)
+    return next();
+  else
+    return res.sendStatus(401);
+};
+ 
+//Login
+//La sesión será diferente para cada usuario, y también será única para el mismo usuario utilizando diferentes navegadores.
+app.get('/login', function (req, res) {
+  if (!req.query.username || !req.query.password) {
+    res.send('login failed');
+  } else if(req.query.username === "amy" || req.query.password === "amyspassword") {
+    req.session.user = "amy";
+    req.session.admin = true;
+    res.send("login success!");
+  }
 });
+ 
+//Logout
+//Destruye la sesión.
+app.get('/logout', function (req, res) {
+  req.session.destroy();
+  res.send("logout success!");
+});
+ 
+//Get
+//Obtiene el contenido protegido. La función de autenticación anterior se pasa en el segundo parámetro como middleware antes de proceder a servir el contenido al usuario. Si la función auth determinó que el usuario no es válido, no pasará a la tercera función para servir el contenido.
+app.get('/content', auth, function (req, res) {
+    res.send("You can only see this after you've logged in.");
+});
+ 
+//Iniciamos la aplicación escuchando en el puerto 8088.
+app.listen(8088);
+console.log("app running at http://localhost:8088");
